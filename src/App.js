05a0538824c9a17355405aeb4cf2e36012b16ecc
Leksa <admin@petiteweb.ru>
2023-06-07 21:18:04 +0300

    Добавли обработчик на Mode и ссылку на результат

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import News from "./components/news"
 
 function App() {
+  console.log(process.env);
   return <>
     <h1>Проект с новосями по API <a href="https://newsapi.org" target="_blank" rel="noreferrer">
       newsapi
     </a></h1>
-    <News/>
+    { process.env.NODE_ENV === "development"
+      ? <News/>
+      : <p>Вы можете работать с API для новостей только на персональном компьютере через localhost</p>
+    }
   </>
 }
 
